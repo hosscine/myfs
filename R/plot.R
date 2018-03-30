b0d11916bed2d411537c5f84ab1b16fe8a224c16
hosscine <futagamirentaro@gmail.com>
2018-03-30 12:51:25 +0900

    Add setMargin to plot

---
+++ R/plot.R
@@ -0,0 +1,12 @@
+
+#' Changes margins for plot
+#'
+#' @param top.4 maring of top. default is 4.
+#' @param left.4 margin of left. defualt is 4.
+#' @param bottom.5 margin of bottom. default is 5.
+#' @param right.2 margin of right. default is 2.
+#'
+#' @export
+#'
+setMargin <- function(top.4 = 4, left.4 = 4, bottom.5 = 5, right.2 = 2)
+  graphics::par(mar=c(bottom.5, left.4, top.4, right.2))
