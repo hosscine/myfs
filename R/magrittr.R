dccf489ce567dd6630a0b66988ab85731aaad7a2
hosscine <futagamirentaro@gmail.com>
2018-06-16 02:51:41 +0900

    Add passign in magrittr

---
+++ R/magrittr.R
@@ -0,0 +1,28 @@
+#' assign function for pipe stream
+#'
+#' @param value a value to be assigned to \code{x}.
+#' @param x a variable name.
+#' @param function_list a list of functions.
+#' @param envir the environment to use.
+#'
+#' @importFrom magrittr freduce
+#'
+#' @return \code{value} with applied function_list.
+#' @export
+#'
+#' @examples
+#' x <- 1:4
+#' x.sum <- x %>% passign(x.sqrt, function_list = list(sqrt)) %>% sum
+#'
+#' print(x.sum)
+#' print(x.sqrt)
+#'
+passign <- function(value, x, function_list = list(), envir = globalenv()){
+  if (length(function_list) == 0)
+    assign(x = as.character(substitute(x)), value = value, envir = envir)
+  else {
+    new.value <- freduce(value, function_list)
+    assign(x = as.character(substitute(x)), value = new.value, envir = envir)
+  }
+  return(value)
+}
