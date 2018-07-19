20a365a55bae1b7cdbb6d02b8db3d96d357a58f0
hosscine <futagamirentaro@gmail.com>
2018-07-19 21:20:21 +0900

    Add preparePrintify function

---
+++ R/Rnotebook.R
@@ -0,0 +1,20 @@
+
+#' Prepare the process that enables to print out your R-notebook document more beautifully
+#'
+#' enable to background color of code chunks and remove "hide" button.
+#'
+#' @export
+#'
+preparePrintify <- function(){
+  utils::download.file("https://gist.githubusercontent.com/penpenpng/b6f0e157d09ccce9ac4f6b0cdfe1a2f9/raw/printify.html",
+                       destfile = "printify.html")
+  cat("add YAML header of your R-notebook belows:
+*************
+output:
+  html_notebook:
+    includes:
+      after_body: printify.html
+*************\n\n")
+
+  cat("this printify process is contributed by penpenpng (https://github.com/penpenpng).\n")
+}
